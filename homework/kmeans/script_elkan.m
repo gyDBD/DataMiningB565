fe922819a435e6e435d493dfe285505464276f42
Rachel <gydbd06@hotmail.com>
2018-04-16 23:33:01 -0400

    Update and rename scriptElkan.m to script_elkan.m

--- homework/kmeans/script_elkan.m
+++ homework/kmeans/script_elkan.m
@@ -3,13 +3,13 @@ T = A(:,:);
 X = table2array(T);
 
 K = 3;
-centroids = initCentroids(X, K);
+centroids = init_centroids(X, K);
 number = 0;
 numberDistance = 0;
 while (true) 
     number = number + 1;
     s = getBetweenCentroids(centroids);
-    [indices,number2] = getClosestCentroids(X, centroids);
+    [indices,number2] = get_closest_centroids(X, centroids);
     numberDistance = numberDistance + number2;
     [newcentroids,number3] = getl(X,centroids,s,indices);
     numberDistance = numberDistance + number3;
